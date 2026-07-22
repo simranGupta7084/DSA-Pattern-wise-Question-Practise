769e60f23f5c5e753c736138c8f9df491e4d685c
Simran <guptayashi705@gmail.com>
2026-07-22 13:06:16 +0530

    Time: 7 ms (91.3%), Space: 35.2 MB (19.67%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -18,10 +18,10 @@ public:
         int low = *max_element(weights.begin(),weights.end());
         int high = accumulate(weights.begin(),weights.end(),0);
  
-        while(low<high){
+        while(low<=high){
             int mid = low+(high-low)/2;
             if(Nodays(weights,mid ) <= days){
-                high = mid;
+                high = mid-1;
             }else{
                 low = mid+1;
             }
